ae754aa1cf6ef988727c66cd5aa0e1e8970ae05b
Shwetank <mshwetank@gmail.com>
2025-08-14 12:13:41 +0530

    feature: fixed css

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        imageUrl: "https://imgcp.aacdn.jp/img-a/1440/auto/global-aaj-front/article/2015/12/565f05f621364_565f018e5feb3_1467636135.jpg"
     },
     {
         title: "Burj Khalifa",
@@ -33,7 +33,7 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+        imageUrl: "https://images.unsplash.com/photo-1705512604302-3dc9d171832c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2VpcmFuZ2VyZmpvcmR8ZW58MHx8MHx8fDA%3D"
     },
     {
         title: "Sydney Opera House",
@@ -42,6 +42,6 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
-        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+        imageUrl: "https://meanjin.com.au/wp-content/uploads/2020/07/McCulloch.jpg"
     }
 ]
